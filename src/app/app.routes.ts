33edae8e8dfcc9228c68de35578818c0eb07181b
Jean-Baptiste Sainte-Beuve <saintebeuve@gmail.com>
2025-01-27 14:09:44 +0100

    Ajout de la route pour le composant du panier d'achat

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ProductGridComponent } from './components/product-grid/product-grid.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 export const routes: Routes = [
   {
@@ -12,5 +13,10 @@ export const routes: Routes = [
     path: 'product/:id',
     title: 'ProductDetail',
     component: ProductDetailComponent,
+  },
+  {
+    path: '/shopping-cart',
+    title: 'ShoppingCart',
+    component: ShoppingCartComponent,
   },
 ];
