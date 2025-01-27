6177d8897ab4a302989f8356ee1b2cd7e8e2129a
Quantix06 <marchalquentin06@gmail.com>
2025-01-27 14:05:40 +0100

    modification on shopping cart
    modification

--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -8,5 +9,19 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class ShoppingCartComponent {
-
+  data = localStorage.getItem('ng_hp_cart');
+  products: Product[] = this.data ? JSON.parse(this.data) : [];
+  cart: Product[] = [...this.products];
+  total: number = this.cart.reduce((sum, product) => sum + product.price, 0);
+  qte: number = 1;
+  removeFromCart(product: Product) {
+    // Filter out the product to remove it from the cart
+    this.cart = this.cart.filter((p: Product) => p.id !== product.id);
+  
+    // Update the total price
+    this.total = this.cart.reduce((sum, p) => sum + p.price, 0);
+  
+    // Update the localStorage after removing the product
+    localStorage.setItem('ng_hp_cart', JSON.stringify(this.cart));
+  }  
 }
